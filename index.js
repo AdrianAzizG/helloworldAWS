3531f8b7e2a72ac4fd10f7539ea0d815a63d64ca
Adrian Gunawan <azizadrian9@gmail.com>
2024-02-01 14:39:33 +0700

    TB-3 commit to JIRA

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From tim1-2" }');
+    res.send('{ "response": "Hello From Tim1" }');
 });
 
 app.get('/will', function (req, res) {
